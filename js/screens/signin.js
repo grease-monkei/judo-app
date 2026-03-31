f19825a7c2e7c9aa6fc21e5618b3997acde84684
Tim Masters <tmasters@d01.local>
2026-03-31 10:09:12 -0700

    Restore sign-in time constraints after successful testing

--- js/screens/signin.js
+++ js/screens/signin.js
@@ -56,14 +56,12 @@ const SignInScreen = (() => {
             
             // Only highlight NEXT class if it's within 2 hours (120 mins)
             let nextClass = Utils.detectNextClass(allSchedules, currentClass, now);
-            /*
             if (nextClass) {
                 const [nH, nM] = nextClass.startTime.split(':').map(Number);
                 if ((nH * 60 + nM) - nowMins > 120) {
                     nextClass = null; // Too early to highlight
                 }
             }
-            */
             
             const isHighlighting = !!currentClass || !!nextClass;
 
